f7a2e279f3ba6d9c4f8a96b55d0fe8655415b443
Iro Kasidiari <irokasidiari@MacBook-Pro-Iro.lan>
2018-12-27 11:51:13 +0200

    profile changes

--- editor/views.py
+++ editor/views.py
@@ -5,11 +5,18 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic import UpdateView
+from django.utils.decorators import method_decorator
+#from .models import Profile
+
 
 class IndexView(generic.ListView):
     template_name = 'editor/index.html'
     def get_queryset(self):
         return
+"""
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -23,3 +30,27 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+"""
+
+"""
+class ProfileObjectMixin(SingleObjectMixin):
+
+    model = Profile
+
+    def get_object(self):
+        try:
+            return self.request.user.get_profile()
+        except Profile.DoesNotExist:
+            raise NotImplemented(
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        klass = ProfileObjectMixin
+        return super(klass, self).dispatch(request, *args, **kwargs)
+"""
+"""
+class ProfileUpdateView(ProfileObjectMixin, UpdateView):
+
+    pass  # That's All Folks!
+    """
+    
